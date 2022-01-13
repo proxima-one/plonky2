fa3a81172aa67a66b31b81db7197c0c3d4a69df1
Sebastien La Duca <sladuca777@gmail.com>
2022-01-13 11:49:00 -0800

    use .into() in examples

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
 	/// f(x) = x^2 + x + 1
 	/// g(x) = 6x^2 + 2x + 3
 	/// where a is a constant
-	fn example_circuit_0(_a: u32) -> (Target, Target, CircuitData<F, C, D>) {
+	fn example_circuit_0(a: u32) -> (Target, Target, CircuitData<F, C, D>) {
 		// start with the standard cirucit config. It's probably way larger than necessary
 		let config = CircuitConfig::standard_recursion_config();
 
@@ -28,17 +28,17 @@ mod tests {
 		builder.register_public_input(x);
 
 		// f(x)
-		let x_squared_plus_x = builder.arithmetic(F::from(1u32), F::from(1u32), x, x, x);
-		let f_x = builder.add_const(x_squared_plus_x, F::from(1u32));
+		let x_squared_plus_x = builder.arithmetic(1u32.into(), 1u32.into(), x, x, x);
+		let f_x = builder.add_const(x_squared_plus_x, 1u32.into());
 
 		// g(x)
-		let six_x_squared_plus_two_x = builder.arithmetic(F::from(6u32), F::from(2u32), x, x, x);
-		let g_x = builder.add_const(six_x_squared_plus_two_x, F::from(3u32));
+		let six_x_squared_plus_two_x = builder.arithmetic(6u32.into(), 2u32.into(), x, x, x);
+		let g_x = builder.add_const(six_x_squared_plus_two_x, 3u32.into());
 
 		// x == a
 
-		let a = builder.constant(F::from(_a));
-		let x_minus_a = builder.sub(x, a);
+		let _a = builder.constant(a.into());
+		let x_minus_a = builder.sub(x, _a);
 		// comparison works on lower-radix number systems. Gate degree for the splitter is the limb base,
 		// so choose binary because it gives the smallest degree.
 		// convert to binary by splitting x_minus_a into 64 base-2 limbs
@@ -62,8 +62,8 @@ mod tests {
 		// set the public inputs via a PartialWitness
 		// set x = 42. Then x == 0, so y should be f(x) = 1807
 		let mut pw = PartialWitness::new();
-		pw.set_target(x, F::from(42u32));
-		pw.set_target(y, F::from(1807u32));
+		pw.set_target(x, 42u32.into());
+		pw.set_target(y, 1807u32.into());
 
 		// prove
 		let proof = circuit.prove(pw)?;
@@ -75,8 +75,8 @@ mod tests {
 		// make another proof with different inputs.
 		// this time, x = 420 => y should be g(x) = 1059243
 		let mut pw = PartialWitness::new();
-		pw.set_target(x, F::from(420u32));
-		pw.set_target(y, F::from(1059243u32));
+		pw.set_target(x, 420u32.into());
+		pw.set_target(y, 1059243u32.into());
 
 		let proof = circuit.prove(pw)?;
 		circuit.verify(proof).expect("expected verifier to accept");
@@ -92,8 +92,8 @@ mod tests {
 
 		// set x = 21. 21 != 6, so y should be g(x) = 2691, but set it to f(x) = 463 instead.
 		let mut pw = PartialWitness::new();
-		pw.set_target(x, F::from(21u32));
-		pw.set_target(y, F::from(463u32));
+		pw.set_target(x,21u32.into());
+		pw.set_target(y, 463u32.into());
 		let _proof_should_panic = circuit.prove(pw);
 	}
 }
