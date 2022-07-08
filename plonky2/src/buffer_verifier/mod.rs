8a4b85858280bebbd681cd6d0084ffa03e47f0b2
Sebastien La Duca <sladuca777@gmail.com>
2022-07-08 16:40:18 -0400

    port verify_constraints

--- plonky2/src/buffer_verifier/mod.rs
+++ plonky2/src/buffer_verifier/mod.rs
@@ -1,9 +1,11 @@
 pub(crate) mod fri_verifier;
 pub mod plonk_verifier;
 
-pub(crate) mod buf;
 pub mod circuit_buf;
 pub mod proof_buf;
 pub mod serialization;
+
+pub(crate) mod buf;
 pub(crate) mod get_challenges;
+pub(crate) mod vanishing_poly;
 pub(crate) mod util;
--- plonky2/src/buffer_verifier/plonk_verifier.rs
+++ plonky2/src/buffer_verifier/plonk_verifier.rs
@@ -11,7 +11,7 @@ use crate::plonk::circuit_data::{CommonCircuitData, VerifierOnlyCircuitData};
 use crate::plonk::config::{GenericConfig, Hasher};
 use crate::plonk::plonk_common::reduce_with_powers;
 use crate::plonk::proof::{Proof, ProofChallenges, OpeningSet};
-use crate::plonk::vanishing_poly::eval_vanishing_poly;
+use crate::buffer_verifier::vanishing_poly::eval_vanishing_poly;
 use crate::plonk::vars::EvaluationVars;
 
 pub(crate) fn verify<'a, 'b, C: GenericConfig<D>, const D: usize>(
@@ -99,35 +99,43 @@ where
     Ok(())
 }
 
-pub(crate) fn verify_with_challenges<
-    F: RichField + Extendable<D>,
-    C: GenericConfig<D, F = F>,
+pub(crate) fn verify_constraints<
+    'a,
+    'b,
+    C: GenericConfig<D>,
     const D: usize,
 >(
-    proof: Proof<F, C, D>,
-    public_inputs_hash: <<C as GenericConfig<D>>::InnerHasher as Hasher<F>>::Hash,
-    challenges: ProofChallenges<F, D>,
-    verifier_data: &VerifierOnlyCircuitData<C, D>,
-    common_data: &CommonCircuitData<F, C, D>,
+    proof_buf: &mut ProofBuf<C, &'a mut [u8], D>,
+    circuit_buf: &mut CircuitBuf<C, &'b mut [u8], D>,
+    challenges: &ProofChallenges<C::F, D>,
+    openings: &OpeningSet<C::F, D>,
+    pis_hash: <<C as GenericConfig<D>>::InnerHasher as Hasher<C::F>>::Hash,
+    degree_bits: usize,
+    quotient_degree_factor: usize,
+    num_partial_products: usize,
+    num_gate_constraints: usize,
+    num_routed_wires: usize,
+    num_challenges: usize,
 ) -> Result<()>
 where
     [(); C::Hasher::HASH_SIZE]:,
 {
-    let local_constants = &proof.openings.constants;
-    let local_wires = &proof.openings.wires;
     let vars = EvaluationVars {
-        local_constants,
-        local_wires,
-        public_inputs_hash: &public_inputs_hash,
+        local_constants: &openings.constants,
+        local_wires: &openings.wires,
+        public_inputs_hash: &pis_hash,
     };
-    let local_zs = &proof.openings.plonk_zs;
-    let next_zs = &proof.openings.plonk_zs_next;
-    let s_sigmas = &proof.openings.plonk_sigmas;
-    let partial_products = &proof.openings.partial_products;
+
+    let local_zs = &openings.plonk_zs;
+    let next_zs = &openings.plonk_zs_next;
+    let s_sigmas = &openings.plonk_sigmas;
+    let partial_products = &openings.partial_products;
 
     // Evaluate the vanishing polynomial at our challenge point, zeta.
-    let vanishing_polys_zeta = eval_vanishing_poly(
-        common_data,
+    let k_is = circuit_buf.read_k_is(num_routed_wires)?;
+    let selectors_info = circuit_buf.read_selectors_info()?;
+    let gates = circuit_buf.read_gates()?;
+    let vanishing_polys_zeta = eval_vanishing_poly::<C::F, C, D>(
         challenges.plonk_zeta,
         vars,
         local_zs,
@@ -137,41 +145,34 @@ where
         &challenges.plonk_betas,
         &challenges.plonk_gammas,
         &challenges.plonk_alphas,
+        gates.as_slice(),
+        &selectors_info,
+        k_is.as_slice(),
+        degree_bits,
+        quotient_degree_factor,
+        num_partial_products,
+        num_gate_constraints,
+        num_challenges,
+        num_routed_wires
     );
 
     // Check each polynomial identity, of the form `vanishing(x) = Z_H(x) quotient(x)`, at zeta.
-    let quotient_polys_zeta = &proof.openings.quotient_polys;
+    let quotient_polys_zeta = &openings.quotient_polys;
     let zeta_pow_deg = challenges
         .plonk_zeta
-        .exp_power_of_2(common_data.degree_bits);
-    let z_h_zeta = zeta_pow_deg - F::Extension::ONE;
+        .exp_power_of_2(degree_bits);
+    let z_h_zeta = zeta_pow_deg - C::FE::ONE;
     // `quotient_polys_zeta` holds `num_challenges * quotient_degree_factor` evaluations.
     // Each chunk of `quotient_degree_factor` holds the evaluations of `t_0(zeta),...,t_{quotient_degree_factor-1}(zeta)`
     // where the "real" quotient polynomial is `t(X) = t_0(X) + t_1(X)*X^n + t_2(X)*X^{2n} + ...`.
     // So to reconstruct `t(zeta)` we can compute `reduce_with_powers(chunk, zeta^n)` for each
     // `quotient_degree_factor`-sized chunk of the original evaluations.
     for (i, chunk) in quotient_polys_zeta
-        .chunks(common_data.quotient_degree_factor)
+        .chunks(quotient_degree_factor)
         .enumerate()
     {
         ensure!(vanishing_polys_zeta[i] == z_h_zeta * reduce_with_powers(chunk, zeta_pow_deg));
     }
-
-    let merkle_caps = &[
-        verifier_data.constants_sigmas_cap.clone(),
-        proof.wires_cap,
-        proof.plonk_zs_partial_products_cap,
-        proof.quotient_polys_cap,
-    ];
-
-    verify_fri_proof::<F, C, D>(
-        &common_data.get_fri_instance(challenges.plonk_zeta),
-        &proof.openings.to_fri_openings(),
-        &challenges.fri_challenges,
-        merkle_caps,
-        &proof.opening_proof,
-        &common_data.fri_params,
-    )?;
 
     Ok(())
 }
