86c9149791309274120458b1d30a7d6f031eefca
Sebastien La Duca <sladuca777@gmail.com>
2022-08-03 21:36:12 -0400

    add x15 bench

--- merkle-stark/benches/sha256_compression.rs
+++ merkle-stark/benches/sha256_compression.rs
@@ -8,15 +8,16 @@ use merkle_stark::{
 use plonky2::hash::hash_types::BytesHash;
 use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 use plonky2::util::timing::TimingTree;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 const D: usize = 2;
 type C = PoseidonGoldilocksConfig;
 type F = <C as GenericConfig<D>>::F;
 type S = Sha2CompressionStark<F, D>;
 
-const NUM_HASHES: usize = 63;
+const NUM_HASHES: usize = 15;
 
-fn main() {
+fn bench_sha256_x16(c: &mut Criterion) {
     let mut builder = env_logger::Builder::from_default_env();
     builder.format_timestamp(None);
     builder.filter_level(LevelFilter::Debug);
@@ -36,9 +37,14 @@ fn main() {
     config.fri_config.cap_height = 4;
 
     let stark = S::new();
-    let mut timing = TimingTree::new("prove", Level::Debug);
-    let proof = prove::<F, C, S, D>(stark, &config, trace, [], &mut timing).unwrap();
-    timing.print();
 
-    verify_stark_proof(stark, proof, &config).unwrap();
+    let mut timing = TimingTree::default();
+	c.bench_function("sha256_compress_x16", |b| {
+		b.iter_batched(|| trace.clone(), |trace| {
+			prove::<F, C, S, D>(stark, &config, trace, [], &mut timing).unwrap();
+		}, criterion::BatchSize::LargeInput);
+	});
 }
+
+criterion_group!(benches, bench_sha256_x16);
+criterion_main!(benches);
