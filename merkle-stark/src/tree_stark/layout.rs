1776551054c8687e27118d6a6042fbf2121cfdfd
Sladuca <sladuca777@gmail.com>
2022-08-08 17:25:15 -0400

    start tree stark

--- merkle-stark/src/tree_stark/layout.rs
+++ merkle-stark/src/tree_stark/layout.rs
@@ -3,9 +3,12 @@ pub const TREE_WIDTH: usize = 1 << (TREE_DEPTH - 1);
 pub const WORDS_PER_HASH: usize = 8;
 
 pub const NUM_PUBLIC_INPUTS: usize = TREE_WIDTH * WORDS_PER_HASH;
+pub const NUM_COLS: usize = LAST_COL + 1;
 
 pub const INPUT_FILTER: usize = 0;
-pub const OUTPUT_FILTER: usize = 1;
+pub const OUTPUT_FILTER: usize = INPUT_FILTER + 1;
+pub const DEPTH_CTR: usize = OUTPUT_FILTER + 1;
+
 pub const VALS_START: usize = 0;
 pub fn val_i_word(i: usize, word: usize) -> usize {
 	VALS_START + i * 8 + word
@@ -25,3 +28,5 @@ pub const HASH_OUTPUT: usize = HASH_INPUT_1 + WORDS_PER_HASH;
 pub fn hash_output_word(word: usize) -> usize {
 	HASH_OUTPUT + word
 }
+
+pub const LAST_COL: usize = HASH_OUTPUT + WORDS_PER_HASH;
--- merkle-stark/src/tree_stark/mod.rs
+++ merkle-stark/src/tree_stark/mod.rs
@@ -1,1 +1,81 @@
-// TODO
+use std::marker::PhantomData;
+use plonky2::field::{polynomial::PolynomialValues, types::Field};
+use plonky2::{
+    field::{
+        extension::{Extendable, FieldExtension},
+        packed::PackedField,
+    },
+    hash::hash_types::RichField,
+    plonk::circuit_builder::CircuitBuilder,
+};
+use crate::{
+    constraint_consumer::{ConstraintConsumer, RecursiveConstraintConsumer},
+    stark::Stark,
+    vars::{StarkEvaluationTargets, StarkEvaluationVars},
+};
+
+// pub mod generation;
+pub mod layout;
+
+use layout::*;
+
+#[derive(Copy, Clone)]
+pub struct MerkleTree5STARK<F: RichField + Extendable<D>, const D: usize> {
+    _phantom: PhantomData<F>,
+}
+
+impl<F: RichField + Extendable<D>, const D: usize> MerkleTree5STARK<F, D> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+
+impl<F: RichField + Extendable<D>, const D: usize> Stark<F, D> for MerkleTree5STARK<F, D> {
+    const COLUMNS: usize = NUM_COLS;
+    const PUBLIC_INPUTS: usize = NUM_PUBLIC_INPUTS;
+
+    fn eval_packed_generic<FE, P, const D2: usize>(
+        &self,
+        vars: StarkEvaluationVars<FE, P, { Self::COLUMNS }, { Self::PUBLIC_INPUTS }>,
+        yield_constr: &mut ConstraintConsumer<P>,
+    ) where
+        FE: FieldExtension<D2, BaseField = F>,
+        P: PackedField<Scalar = FE>,
+    {
+		let curr_row = vars.local_values;
+		let next_row = vars.next_values;
+		let pis = vars.public_inputs;
+
+		// load leaves in at first row
+		for i in 0..TREE_WIDTH {
+			for word in 0..WORDS_PER_HASH {
+				yield_constr.constraint_first_row(
+					curr_row[val_i_word(i, word)] - pis[i * WORDS_PER_HASH + word]
+				);
+			}
+		}
+
+		// set depth counter to 0 first row
+		yield_constr.constraint_first_row(curr_row[DEPTH_CTR]);
+
+		// compress leftmost two hashes, rotate words left, by two, and 
+		
+	}
+
+
+    fn eval_ext_circuit(
+        &self,
+        _builder: &mut CircuitBuilder<F, D>,
+        _vars: StarkEvaluationTargets<D, { Self::COLUMNS }, { Self::PUBLIC_INPUTS }>,
+        _yield_constr: &mut RecursiveConstraintConsumer<F, D>,
+    ) {
+        todo!()
+    }
+
+    fn constraint_degree(&self) -> usize {
+        3
+    }
+}
