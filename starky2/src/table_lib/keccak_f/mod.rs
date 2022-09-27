804d8021e6ca7738dc1c76e8f69a34958e19c810
Sladuca <sladuca777@gmail.com>
2022-09-27 15:38:26 -0400

    copy over keccak stark

---
+++ starky2/src/table_lib/keccak_f/mod.rs
@@ -0,0 +1,5 @@
+pub mod layout;
+pub mod constants;
+pub mod keccak_stark;
+pub mod logic;
+pub mod round_flags;
--- starky2/src/table_lib/mod.rs
+++ starky2/src/table_lib/mod.rs
@@ -1,2 +1,3 @@
 pub mod depth_5_merkle_tree;
 pub mod sha2_compression;
+pub mod keccak_f;
